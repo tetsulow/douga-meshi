e8c2d561da2b5a70bc1a8efaf47db70a12433ab6
tetsulow <tetsu.miyawaki@gmail.com>
2022-03-26 10:26:16 +0000

    Add noImage

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -10,6 +10,10 @@ class Recipe < ApplicationRecord
   validates :caption,presence:true,length:{maximum:200}
 
   def get_image
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpeg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
     image
   end
 
