81d647ae8da90bc719ccd4c2749345deefeb86a2
tetsulow <tetsu.miyawaki@gmail.com>
2022-04-07 14:06:12 +0000

    Update ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
     root_path
   end
 
-  #ログアウト後はトップページに遷移する
+  #ログアウト後はトップページに遷移
   def after_sign_out_path_for(resource)
     root_path
   end
