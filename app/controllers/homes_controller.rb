29199c14fdb839f69aa9a995046f25d6cd395368
tetsulow <tetsu.miyawaki@gmail.com>
2022-04-08 14:02:43 +0000

    Update ApplicationController

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -4,7 +4,7 @@ class HomesController < ApplicationController
 
   def top
     @recipes = Recipe.order(created_at: :desc).limit(5)
-    @genres = Genre.order("RANDOM()").limit(7)
+    @genres = Genre.order("RANDOM()").limit(7) #タグをランダムで７つ表示
   end
 
 end
